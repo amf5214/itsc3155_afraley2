05d536d853d70d3c70ccf765598f997815761209
amf5214 <81787764+amf5214@users.noreply.github.com>
2023-09-03 23:44:56 -0400

    Fixed bug in script_2.py that caused the primary function to return none

--- script_2.py
+++ script_2.py
@@ -3,7 +3,7 @@
 # 09/03/2023
 # Intermediate Python Exercises 1 
 
-def add_dictionaries(dict1=None, dict2=None):
+def add_dictionaries(dict1, dict2):
 
     """
     Function to take in two dictionaries and outputs a new dictionary with the value elements added for the shared key values
@@ -18,6 +18,12 @@ def add_dictionaries(dict1=None, dict2=None):
         for key, value in dict2.items():
             if key in dict1:
                 rtn_dict[key] = dict1[key] + value
+        
+        return rtn_dict
+    
+    else:
+        return None
+
 
 if __name__ == "__main__":
     dict1 = {"a":5, "b":9, "c":90, "d":34}
