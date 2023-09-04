faea87ae9d7495c4c3cb2f574d5022424ff917fd
amf5214 <81787764+amf5214@users.noreply.github.com>
2023-09-03 20:29:42 -0400

    Add file holding the function to perform addition on unique key values

---
+++ script_2.py
@@ -0,0 +1,15 @@
+# INFO
+# Austin Fraley
+# 09/03/2023
+# Intermediate Python Exercises 1 
+
+def add_dictionaries(dict1=None, dict2=None):
+
+    """
+    Function to take in two dictionaries and outputs a new dictionary with the value elements added for the shared key values
+    @arg dict1 dictionary object
+    @arg dict2 dictionary object
+    @return rtn_dict dictionary object
+    """
+
+    return None
